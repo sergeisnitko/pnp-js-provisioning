7dc515c94249f9d024e3ed74350d339a0accf350
olemp <olemp@puzzlepart.com>
2017-03-27 13:56:40 +0200

    Support for tokens wpgallery and themegallery in util ReplaceTokens

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,7 @@
 const ReplaceTokens = (str: string): string => {
-    return str.replace(/{sitecollection}/g, _spPageContextInfo.siteAbsoluteUrl);
+    return str.replace(/{sitecollection}/g, _spPageContextInfo.siteAbsoluteUrl)
+        .replace(/{wpgallery}/g, `${_spPageContextInfo.siteAbsoluteUrl}/_catalogs/wp`)
+        .replace(/{themegallery}/g, `${_spPageContextInfo.siteAbsoluteUrl}/_catalogs/theme/15`);
 };
 
 const MakeUrlRelative = (absUrl: string): string => {
