ff6965896ace9115f4cc9d5beefd39be73205235
olemp <olemp@puzzlepart.com>
2017-05-15 18:47:54 +0200

    Added SiteLogoUrl IWebSettings

--- src/schema.ts
+++ src/schema.ts
@@ -69,6 +69,7 @@ export interface IWebSettings {
     RecycleBinEnabled?: boolean;
     TreeViewEnabled?: boolean;
     QuickLaunchEnabled?: boolean;
+    SiteLogoUrl?: string;
 
     [key: string]: string | boolean;
 }
